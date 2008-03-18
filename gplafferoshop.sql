45a9bd2a6af61758e166ca0fd75d6f86ae6c8b2a
Roman Frolow <rofrol@gmail.com>
2008-03-18 11:58:43 +0100

    *_id -> id

--- gplafferoshop.sql
+++ gplafferoshop.sql
@@ -2,16 +2,20 @@ DROP DATABASE IF EXISTS gplafferoshop;
 CREATE DATABASE gplafferoshop;
 USE gplafferoshop;
 CREATE TABLE products (
-   products_id int NOT NULL auto_increment,
+   id int NOT NULL auto_increment,
    name varchar(128) NOT NULL,
-   PRIMARY KEY (products_id)
+   PRIMARY KEY (id)
 );
 CREATE TABLE customers (
-   customers_id int NOT NULL auto_increment,
+   id int NOT NULL auto_increment,
    name varchar(128) NOT NULL,
-   PRIMARY KEY (customers_id)
+   PRIMARY KEY (id)
 );
 
 INSERT INTO products VALUES ( '1', 'lada');
 
+INSERT INTO products VALUES ( '2', 'piec');
+
 INSERT INTO customers VALUES ( '1', 'Jan');
+
+INSERT INTO customers VALUES ( '2', 'Marek');
