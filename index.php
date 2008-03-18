99fe6edea6a23b11f01c9642536b6ea0dafab73f
Roman Frolow <rofrol@gmail.com>
2008-03-18 11:01:23 +0100

    wyswietlenie wynikow

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-/* http://pl.php.net/manual/en/function.mysqli-connect.php */
+/* http://pl.php.net/manual/en/function.mysqli-connect.php
+http://www.devshed.com/c/a/MySQL/Implementing-Additional-Methods-with-mysqli-and-PHP-5/1/
+PHP 5 Power Programming -> 6.2.6 Queries
+ */
 $mysqli = new mysqli("localhost", "root", "droot", "gplafferoshop");
 
 /* check connection */
@@ -8,11 +11,15 @@ if (mysqli_connect_errno()) {
   exit();
  }
 
-printf("Host information: %s\n", $mysqli->host_info);
+printf("Host information: %s\n<br>", $mysqli->host_info);
 
-$q = 'SELECT * FROM products';
-$r = mysqli_query($mysqli, $q);
+/* Display results as associative arrays */
+if($result=$mysqli->query("SELECT * FROM products")){
+  while($row=$result->fetch_array(MYSQLI_ASSOC)){
+    echo 'id: ' . $row['id'] . ', name: ' . $row['name'] . '<br>';
+  }
+  $result->free();
+ }
 
-/* close connection */
 $mysqli->close();
 ?>
