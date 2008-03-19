e50515b63ee0db08345f6b24f704272ffe6d121b
Roman Frolow <rofrol@gmail.com>
2008-03-19 15:28:25 +0100

    1. zwraca obiekt połączenia z bazą jeśli istnieje. 2. style "bsd"

--- database.php
+++ database.php
@@ -1,10 +1,25 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "droot", "gplafferoshop");
+class database
+{
+	public static function getConn()
+	{
+		static $mysqli;
 
-/* check connection */
-if (mysqli_connect_errno()) {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  exit();
- }
+		if(isset($mysqli))
+		{
+			return $mysqli;
+		}
 
+		$mysqli = new mysqli("localhost", "root", "droot", "gplafferoshop");
+
+		/* check connection */
+		if (mysqli_connect_errno())
+		{
+			printf("Connect failed: %s\n", mysqli_connect_error());
+			exit();
+		}
+
+		return $mysqli;
+	}
+}
 ?>
