712b372ce228cc3ff927d261600e55ea9bfa2d0e
Roman Frolow <rofrol@gmail.com>
2008-03-21 15:01:41 +0100

    działą. Skrypt należy uruchomić tak
    gettext_test.php?locale=pl_PL.utf8
    
    i pokaże się przetłumaczony na polski tekst

--- gettext_test.php
+++ gettext_test.php
@@ -1,13 +1,14 @@
 <?php
   // I18N support information here
-$language = 'en_US.utf8';
-putenv("LANG=$language"); 
-setlocale(LC_ALL, $language);
+$locale = "en_US.utf8";
+if (isSet($_GET["locale"])) $locale = $_GET["locale"];
+putenv("LC_ALL=$locale");
+setlocale(LC_ALL, $locale);
 
 // Set the text domain as 'messages'
 $domain = 'messages';
-bind_textdomain_codeset($domain, ‘UTF-8′);
-bindtextdomain($domain, "/www/htdocs/site.com/locale"); 
+bind_textdomain_codeset($domain, "UTF-8");
+bindtextdomain($domain, "./locale"); 
 textdomain($domain);
 
 echo gettext("A string to be translated would go here");
