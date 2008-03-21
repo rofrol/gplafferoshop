a1152137b4a7ae1190c2da58eee09f5fb4aa8cc9
Roman Frolow <rofrol@gmail.com>
2008-03-21 14:09:27 +0100

    testowe pliki do sprawdzenia xgettext -n *.php

---
+++ gettext_test.php
@@ -0,0 +1,14 @@
+<?php
+  // I18N support information here
+$language = 'en_US.utf8';
+putenv("LANG=$language"); 
+setlocale(LC_ALL, $language);
+
+// Set the text domain as 'messages'
+$domain = 'messages';
+bind_textdomain_codeset($domain, ‘UTF-8′);
+bindtextdomain($domain, "/www/htdocs/site.com/locale"); 
+textdomain($domain);
+
+echo gettext("A string to be translated would go here");
+?>
