de65a5f8ed4ce724f4b96f0d00043013e2d9bb8d
Roman Frolow <rofrol@gmail.com>
2008-07-03 11:20:01 +0200

    linki do podstron

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
+<a href="products.php">products.php</a><br>
+<a href="customers.php">customers.php</a><br>
+<a href="transactions.php">transactions.php</a><br>
+
 <?php
 /* http://pl.php.net/manual/en/function.mysqli-connect.php
 http://www.devshed.com/c/a/MySQL/Implementing-Additional-Methods-with-mysqli-and-PHP-5/1/
-(013147149X_book.pdf) Andi Gutmans, Stig Sæther Bakken,
+(013147149X_book.pdf) Andi Gutmans, Stig S??her Bakken
 and Derick Rethans - PHP 5 Power Programming -> 6.2.6 Queries
 */
 
