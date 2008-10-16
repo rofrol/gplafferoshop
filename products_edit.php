401697b94520da7d0e497e224cce7c4dffae14a2
Roman Frolow <rofrol@gmail.com>
2008-10-16 21:30:52 +0200

    juz dzala zmiana, tylko jeszcze co z php_self jesli modul jest zagniezdzony?

--- products_edit.php
+++ products_edit.php
@@ -5,19 +5,22 @@ class products_edit
 /* Display results as associative arrays */
 	public static function display()
 	{
+		if (isset($_REQUEST['submit']))
+		{
+			$result = database::getConn()->query("UPDATE products SET products_name='$_REQUEST[products_name]', products_price='$_REQUEST[products_price]' WHERE products_id='$_REQUEST[products_id]'");
+		}
+
 		if($result=database::getConn()->query("SELECT * FROM products"))
 		{
-			echo '<form action="products_edit.php" method="get">';
 			echo '<br>products<br>';
 			echo '<table border="1"><thead><tr><th>products_id</th><th>products_name</th><th>products_price</th></tr></thead><tbody>';
 			while($row=$result->fetch_array(MYSQLI_ASSOC))
 			{
-				echo '<tr><td><input type="text" name="text" value="' . $row['products_id'] . '"></td><td><input type="text" name="text" value="' . $row['products_name'] .
-'"</td><td><input type="text" name="text" value="' . $row['products_price'] . '"</td></tr>';
+				echo '<form action="products_edit.php" method="post">';
+				echo '<tr><td><input type="text" readonly="readonly" name="products_id" value="' . $row['products_id'] . '"></td><td><input type="text" name="products_name" value="' . $row['products_name'] .'"></td><td><input type="text" name="products_price" value="' . $row['products_price'] . '"></td><td><input type="submit" name="submit" value="Zmien"></td></tr>';
+				echo '</form>';
 			}
 			echo '</tbody></table>';
-			echo '<input type="submit" name="change" value="Zmien"/>';
-			echo '</form>';
 			$result->free();
 		}
 	}
