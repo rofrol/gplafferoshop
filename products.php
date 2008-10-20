c34b8b9d50907516fd27904f01bdfb08bbd3e592
Roman Frolow <rofrol@gmail.com>
2008-10-20 15:25:44 +0200

    wyswietlanie bez dodatkowej kolumnny col_id

--- products.php
+++ products.php
@@ -8,28 +8,54 @@ function print_row($value, $key) {
 class products
 {
 /* Display results as associative arrays */
-	public static function display($table, $col_id)
+	public static function display($table)
 	{
 		f_products_update();
 
 		if($result=database::getConn()->query("SELECT * FROM $table"))
 		{
-			echo '<br>products<br>';
-			echo '<table border="1"><thead><tr><th>'.$col_id.'</th><th>products_name</th><th>products_price</th></tr></thead><tbody>';
+			echo "<br>$table<br>";
+			echo "<table border=\"1\"><thead>";
+			$i=1;
 			while($row=$result->fetch_array(MYSQL_BOTH))
 			{
-
-				$array_of_keys = array_keys($row);
-				while (list($a, $b) = each($array_of_keys))
+				$array_of_keys=array_keys($row);
+				if($i==1)
 				{
-					echo "<tr><td>a: $a</td><td>b: $b</td><td>row[b]: $row[$b]<td></tr>";
+					echo "<tr>";
+					while(list($a,$b)=each($array_of_keys))
+					{
+						if($a%2!=0)
+						{
+							echo "<th>$b</th>";
+						}
+					}
+					echo "</tr>";
+					$i++;
 				}
+			}
+			echo "</thead>";
+		}
+		if($result=database::getConn()->query("SELECT * FROM $table"))
+		{
+			echo "<tbody>";
+			echo "<form action=\"$_SERVER['PHP_SELF']\" method=\"post\">";
+			while($row=$result->fetch_array(MYSQL_BOTH))
+			{
+				echo "<tr>";
+				$array_of_keys=array_keys($row);
+				while(list($a,$b)=each($array_of_keys))
+				{
+					if($a%2!=0)
+					{
+						echo "<td>$row[$b]</td>";
+					}
+				}
+				echo "</tr>";
 
-				echo "<tr><td>przejscie petli</td></tr>";
 				#array_walk($row, 'print_row');
 /*
 
-				echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
 				echo '<tr><td><input type="text" readonly="readonly" name="products_id" value="' . $row['products_id'] . '"></td><td><input type="text" name="products_name" value="' . $row['products_name'] .'"></td><td><input type="text" name="products_price" value="' . $row['products_price'] . '"></td><td><input type="submit" name="submit" value="Zmien"></td></tr>';
 				echo '</form>';
 */
