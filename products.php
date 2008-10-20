2286cb4168a3728f2e76cb3713c21e5c2e17922c
Roman Frolow <rofrol@gmail.com>
2008-10-20 17:31:43 +0200

    troche  uporzadkowe i skompaktowane

--- products.php
+++ products.php
@@ -11,10 +11,9 @@ class products
 		if($result=database::getConn()->query("SELECT * FROM $table"))
 		{
 			echo '<br>'.$table.'<br>';
-			echo '<table border="1"><thead>';
+			echo '<table border="1"><thead><tr>';
 			$row=$result->fetch_array(MYSQL_BOTH);
 			$array_of_keys=array_keys($row);
-			echo '<tr>';
 			while(list($a,$b)=each($array_of_keys))
 			{
 				if($a%2!=0)
@@ -22,21 +21,20 @@ class products
 					echo '<th>'.$b.'</th>';
 				}
 			}
-			echo '</tr>';
-			echo '</thead>';
-			echo '<tbody>';
+			echo '</tr></thead><tbody>';
 #musze tu zrobic do...while poniewaz each() zmienia chyba pozycje wskaznika w fetch_array i przechodzi sam do nastepnego wiersza
 			do
 			{
-				echo '<form action="'.$_SERVER['PHP_SELF'].'" method="post">';
-				echo '<tr>';
+#TODO dodac filtrowanie argumentow php_self
+				echo '<form action="'.$_SERVER['PHP_SELF'].'" method="post"><tr>';
 				$array_of_keys=array_keys($row);
 				while(list($a,$b)=each($array_of_keys))
 				{
 					if($a%2!=0)
 					{
 						echo '<td><input type="text" name="'.$b.'" value="'.$row[$b].'"';
-						if(ereg("_id$", $b))
+						#aby nie zmieniac identyfikatora
+						if(ereg(".+_id$",$b))
 						{
 							echo ' readonly="readonly"';
 						}
@@ -44,15 +42,13 @@ class products
 					}
 				}
 				echo '<td><input type="submit" name="submit" value="Zmien"></td>';
-				echo '</tr>';
-				echo '</form>';
-			}
-			while($row=$result->fetch_array(MYSQL_BOTH));
+				echo '</tr></form>';
+			}while($row=$result->fetch_array(MYSQL_BOTH));
 			echo '</tbody></table>';
 			$result->free();
 		}
-	} #end function display()
-} #end class products()
+	}#end function display()
+}#end class products()
 
 if(!class_exists(products_loaded))
 {
