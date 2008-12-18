e6fd9693b2500c76032c73ab88d3c28622553918
Roman Frolow <rofrol@gmail.com>
2008-12-18 21:57:21 +0100

    teraz działa dobrze 	modified:   mysql_wczytaj_gplafferoshop.sh

--- mysql_wczytaj_gplafferoshop.sh
+++ mysql_wczytaj_gplafferoshop.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sh mysql_wczytaj.sh gplafferoshop gplafferoshop.sql
+#najpierw trzeba utworzyc baze
+#mysql -h localhost -u root -p
+#mysql> create database gplafferoshop;
+#mysql> quit;
+mysql -h localhost -u root -p --default_character_set utf8 gplafferoshop < gplafferoshop.sql
