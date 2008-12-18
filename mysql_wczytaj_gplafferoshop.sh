67f7930ed92d32b3137fd6187f502239386e99c3
Roman Frolow <rofrol@gmail.com>
2008-12-18 22:39:15 +0100

    skrypty pomocnicze do mysql i rsync 	new file:   mysql_wczytaj_gplafferoshop.sh 	new file:   rsync_gplafferoshop.sh

---
+++ mysql_wczytaj_gplafferoshop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+projs=/mnt/samsung_storage/projects
+sh ${projs}/scripts/mysql_wczytaj.sh gplafferoshop gplafferoshop/gplafferoshop.sql
---
+++ rsync_gplafferoshop.sh
@@ -0,0 +1,3 @@
+rsync -vaWH --delete --exclude=.git /mnt/samsung_storage/projects/gplafferoshop/ /var/www/localhost/htdocs/php5/gplafferoshop/
+chown -R rofrol: /mnt/samsung_storage/projects/gplafferoshop
+chown -R apache: /var/www/localhost/htdocs/php5/gplafferoshop
