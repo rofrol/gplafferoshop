0cab810334db3e753f43d261170948d680c2ff5a
Roman Frolow <rofrol@gmail.com>
2008-12-18 22:04:29 +0100

    check_login 	modified:   login.php

--- login.php
+++ login.php
@@ -2,10 +2,12 @@
 
 class login
 {
+
 /* Display results as associative arrays */
 	public static function display()
 	{
-		echo '
+
+	echo '
 <br>login<br>
 <form action="login.php" method="post">
 <table>
@@ -17,7 +19,62 @@ class login
 </form>
 ';
 	}
-}
+
+	public static function check_login($dbc, $email = '', $pass = '')
+	{
+		if (!empty($errors))
+		{
+			echo '
+<h1>Error!</h1>
+<p class="error">The following error(s) occurred:<br />';
+			foreach ($errors as $msg)
+			{
+				echo " - $msg<br />\n";
+			}
+			echo '</p><p>Please try again.</p>';
+		}
+
+		$errors = array();
+		if (empty($email))
+		{
+			$errors[ ] = 'You forgot to enter your email address.';
+		}
+		else
+		{
+			$e = mysqli_real_escape_string ($dbc, trim($email));
+		}
+		if (empty($pass))
+		{
+			$errors[ ] = 'You forgot to enter your password.';
+		}
+		else
+		{
+			$p = mysqli_real_escape_string($dbc, trim($pass));
+		}
+
+		if (empty($errors))
+		{ // If everything's OK.
+// Retrieve the user_id and first_name for that email/password combination:
+			$q = "SELECT user_id, first_name FROM users WHERE email='$e' AND pass=SHA1('$p')";
+			$r = @mysqli_query ($dbc, $q); // Run the query.
+// Check the result:
+			if (mysqli_num_rows($r) == 1)
+			{
+// Fetch the record:
+				$row = mysqli_fetch_array ($r, MYSQLI_ASSOC);
+// Return true and the record:
+				return array(true, $row);
+			}
+			else
+			{ // Not a match!
+				$errors[] = 'The email address and password entered do not match those on file.';
+			}
+		} // End of empty($errors) IF.
+// Return false and the errors:
+		return array(false, $errors);
+	} // End of check_login() function.
+
+} // End of class products.
 
 if(!class_exists(login_loaded))
 {
