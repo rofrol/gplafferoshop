f99315168b0da6484e70309ce9af0564de42288a
Roman Frolow <rofrol@gmail.com>
2008-12-19 11:29:43 +0100

    $RUNDIRECTORY/../ 	modified:   scripts/rsync_gplafferoshop.sh

--- scripts/rsync_gplafferoshop.sh
+++ scripts/rsync_gplafferoshop.sh
@@ -1,3 +1,5 @@
-rsync -vaWH --delete --exclude=.git ./ /var/www/localhost/htdocs/php5/gplafferoshop/
-chown -R rofrol: .
+#!/bin/bash
+declare -x RUNDIRECTORY="${0%%/*}"
+rsync -vaWH --delete --exclude=.git $RUNDIRECTORY/../ /var/www/localhost/htdocs/php5/gplafferoshop/
+chown -R rofrol: $RUNDIRECTORY/../
 chown -R apache: /var/www/localhost/htdocs/php5/gplafferoshop
