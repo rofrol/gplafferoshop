2275073a3ed09634c38dda9cbbf685f71aca2316
Roman Frolow <rofrol@gmail.com>
2008-12-19 11:20:13 +0100

    $RUNDIRECTORY/gplafferoshop.sql 	renamed:    gplafferoshop.sql -> scripts/gplafferoshop.sql 	modified:   scripts/mysql_wczytaj_gplafferoshop.sh

--- scripts/mysql_wczytaj_gplafferoshop.sh
+++ scripts/mysql_wczytaj_gplafferoshop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#declare -x SCRIPTPATH="${0}"
+declare -x RUNDIRECTORY="${0%%/*}"
+#declare -x SCRIPTNAME="${0##*/}"
+
 #najpierw trzeba utworzyc baze
 #mysql -h localhost -u root -p
 #mysql> create database gplafferoshop;
 #mysql> quit;
-mysql -h localhost -u root -p --default_character_set utf8 gplafferoshop < gplafferoshop.sql
+mysql -h localhost -u root -p --default_character_set utf8 gplafferoshop < $RUNDIRECTORY/gplafferoshop.sql
