e5a62d9c45457bbce6c7a53eeef4e8b1c3f313d2
Roman Frolow <rofrol@gmail.com>
2009-05-12 19:32:12 +0159

    also works when running inside helpers_dev 	modified:   helpers_dev/mysql_wczytaj_gplafferoshop.sh 	modified:   helpers_dev/rsync_gplafferoshop.sh

--- helpers_dev/mysql_wczytaj_gplafferoshop.sh
+++ helpers_dev/mysql_wczytaj_gplafferoshop.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #declare -x SCRIPTPATH="${0}"
 declare -x RUNDIRECTORY="${0%%/*}"
-#declare -x SCRIPTNAME="${0##*/}"
+declare -x SCRIPTNAME="${0##*/}"
+
+if [ "$RUNDIRECTORY" == "$SCRIPTNAME" ]; then
+   RUNDIRECTORY="."
+fi
 
 #najpierw trzeba utworzyc baze
-#mysql -h localhost -u root -p
-#mysql> create database gplafferoshop;
-#mysql> quit;
+mysql -u root -p -e "create database gplafferoshop;"
 mysql -h localhost -u root -p --default_character_set utf8 gplafferoshop < $RUNDIRECTORY/gplafferoshop.sql
+
--- helpers_dev/rsync_gplafferoshop.sh
+++ helpers_dev/rsync_gplafferoshop.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#!/bin/bash
+#declare -x SCRIPTPATH="${0}"
 declare -x RUNDIRECTORY="${0%%/*}"
+declare -x SCRIPTNAME="${0##*/}"
+
+if [ "$RUNDIRECTORY" == "$SCRIPTNAME" ]; then
+   RUNDIRECTORY="."
+fi
+
+if [ ! -d /var/www/localhost/htdocs/php5/gplafferoshop ]; then mkdir -p /var/www/localhost/htdocs/php5/gplafferoshop; fi
 rsync -vaWH --delete --exclude=.git $RUNDIRECTORY/../ /var/www/localhost/htdocs/php5/gplafferoshop/
-chown -R rofrol: $RUNDIRECTORY/../
-chown -R apache: /var/www/localhost/htdocs/php5/gplafferoshop
+#chown -R rofrol: $RUNDIRECTORY/../
+chown -R apache: /var/www/localhost/htdocs/php5/
